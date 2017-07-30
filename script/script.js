9fb380eee70881ab009d3c27cffa142860248951
Salim Oussayfi <box@slimou.net>
2017-07-30 17:29:21 +0200

    script with form validation ans popup functions

--- script/script.js
+++ script/script.js
@@ -26,74 +26,143 @@ function checkForm(){
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
 
-    if (!forname.match(stringPattern)){
+    /*
+    HELFER-FUNKTION
+
+    function validateForm(fieldName, pattern, fieldNameError, message){
+         if (!fieldName.match(pattern) || fieldname == ""){
+             alert(fieldNameError.value);
+             var listPoint = document.createElement('li');
+             errorList.appendChild(listPoint);
+             listPoint.setAttribute('id',fieldNameError);
+             document.getElementById(fieldNameError).className = "error";
+             document.getElementById(fieldNameError).innerHTML = message;
+         }else{
+             if(document.getElementById(fieldNameError)){
+                 listPoint = document.getElementById(fieldNameError);
+                 alert(listPoint);
+                 errorList.removeChild(listPoint);
+             }
+         }
+    };
+
+    validateForm(forname, stringPattern, "fornameError", "Gülltigen Vornamen eingeben");
+    validateForm(surname, stringPattern, "surnameError", "Gülltigen Nachnamen eingeben");
+    */
+
+    //------------------------------------------------------------------------------------------------
+    //FORNAME
+    if (!forname.match(stringPattern) || forname==""){
         var fornameError = document.createElement('li');
         errorList.appendChild(fornameError);
         fornameError.setAttribute('id','fornameError');
         document.getElementById("fornameError").className = "error";
         document.getElementById("fornameError").innerHTML = "Gülltigen Vornamen eingeben";
     }else{
-        var listPoint =  document.getElementById("fornameError");
-            errorList.removeChild(listPoint);
-    }
-
-    if (!surname.match(stringPattern)){
+        if(document.getElementById("fornameError")){
+            var fornameError = document.getElementById("fornameError");
+            errorList.removeChild(fornameError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //SURNAMA
+    if (!surname.match(stringPattern) || surname==""){
+        var surnameError = document.createElement('li');
+        errorList.appendChild(surnameError);
+        surnameError.setAttribute('id','surnameError');
         document.getElementById("surnameError").className = "error";
         document.getElementById("surnameError").innerHTML = "Gülltigen Nachnamen eingeben";
     }else{
-        document.getElementById("surnameError").className = "success";
-        document.getElementById("surnameError").innerHTML = "Nachname: " + surname;
-    }
-
-    if (!street.match(stringPattern)){
+        if(document.getElementById("surnameError")){
+            var surnameError = document.getElementById("surnameError");
+            errorList.removeChild(surnameError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //STREET
+    if (!street.match(stringPattern) || street==""){
+        var streetError = document.createElement('li');
+        errorList.appendChild(streetError);
+        streetError.setAttribute('id','streetError');
         document.getElementById("streetError").className = "error";
         document.getElementById("streetError").innerHTML = "Gülltigen Straßennamen eingeben";
     }else{
-        document.getElementById("streetError").className = "success";
-        document.getElementById("streetError").innerHTML = "Straße: " + street;
-    }
-
-    if(!streetNumber.match(numberPattern)){
+        if(document.getElementById("streetError")){
+            var streetError = document.getElementById("streetError");
+            errorList.removeChild(streetError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //STREET-NUMBER
+    if (!streetNumber.match(numberPattern) || streetNumber==""){
+        var streetNumberError = document.createElement('li');
+        errorList.appendChild(streetNumberError);
+        streetNumberError.setAttribute('id','streetNumberError');
         document.getElementById("streetNumberError").className = "error";
-        document.getElementById("streetNumberError").innerHTML = "Gülltige Hausnummer eingeben";
+        document.getElementById("streetNumberError").innerHTML = "Gülltige Haus-Nummer eingeben";
     }else{
-        document.getElementById("streetNumberError").className = "success";
-        document.getElementById("streetNumberError").innerHTML = "Hausnummer: " + streetNumber;
-    }
-
-    if(!zipCode.match(numberPattern)){
+        if(document.getElementById("streetNumberError")){
+            var streetNumberError = document.getElementById("streetNumberError");
+            errorList.removeChild(streetNumberError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //ZIP-CODE
+    if (!zipCode.match(numberPattern) || zipCode==""){
+        var zipCodeError = document.createElement('li');
+        errorList.appendChild(zipCodeError);
+        zipCodeError.setAttribute('id','zipCodeError');
         document.getElementById("zipCodeError").className = "error";
-        document.getElementById("zipCodeError").innerHTML = "Gülltige Postleitzahl eingeben";
+        document.getElementById("zipCodeError").innerHTML = "Gülltige Postletzahl eingeben";
     }else{
-        document.getElementById("zipCodeError").className = "success";
-        document.getElementById("zipCodeError").innerHTML = "Postleitzahl: " + zipCode;
-    }
-
-    if (!location.match(stringPattern)){
+        if(document.getElementById("zipCodeError")){
+            var zipCodeError = document.getElementById("zipCodeError");
+            errorList.removeChild(zipCodeError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //LOCATION
+    if (!location.match(stringPattern) || location==""){
+        var locationError = document.createElement('li');
+        errorList.appendChild(locationError);
+        locationError.setAttribute('id','locationError');
         document.getElementById("locationError").className = "error";
-        document.getElementById("locationError").innerHTML = "Gülltigen Straßennamen eingeben";
+        document.getElementById("locationError").innerHTML = "Gülltigen Ortsnamen eingeben";
     }else{
-        document.getElementById("locationError").className = "success";
-        document.getElementById("locationError").innerHTML = "Straße: " + location;
-    }
-
-    if (!email.match(emailPattern)) {
+        if(document.getElementById("locationError")){
+            var locationError = document.getElementById("locationError");
+            errorList.removeChild(locationError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //LOCATION
+    if (!email.match(emailPattern) || email==""){
+        var emailError = document.createElement('li');
+        errorList.appendChild(emailError);
+        emailError.setAttribute('id','emailError');
         document.getElementById("emailError").className = "error";
         document.getElementById("emailError").innerHTML = "Gülltige Email-Adresse eingeben";
-    } else{
-            document.getElementById("emailError").className = "success";
-            document.getElementById("emailError").innerHTML = "Email: " + email;
-        }
-
-    if(!phone.match(numberPattern)){
-        document.getElementById("phoneError").className = "error";
-        document.getElementById("phoneError").innerHTML = "Gülltige Telefonnummer eingeben";
     }else{
-        document.getElementById("phoneError").className = "success";
-        document.getElementById("phoneError").innerHTML = "Telefonnummer: " + phone;
-    }
-
-    alert(forname + " " + surname + " " + street + " " + streetNumber + " " + zipCode + " " + location + " " + email + " " + phone);
+        if(document.getElementById("emailError")){
+            var emailError = document.getElementById("emailError");
+            errorList.removeChild(emailError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
+    //PHONE-NUMBER
+    if (!phone.match(numberPattern) || phone==""){
+        var phoneError = document.createElement('li');
+        errorList.appendChild(phoneError);
+        phoneError.setAttribute('id','phoneError');
+        document.getElementById("phoneError").className = "error";
+        document.getElementById("phoneError").innerHTML = "Gülltige Telefon-Nummer eingeben";
+    }else{
+        if(document.getElementById("phoneError")){
+            var phoneError = document.getElementById("phoneError");
+            errorList.removeChild(phoneError);
+        }
+    };
+    //------------------------------------------------------------------------------------------------
 }
 
 
